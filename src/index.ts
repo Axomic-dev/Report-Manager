f62f8e05d9ec71580ebd5aa9ef681e4e491db7fc
VichoSynaptic <vicente.gonzalez@practicante5.local>
2023-02-20 11:06:51 -0300

    feat: Main plan and environment setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,49 @@
+import express from 'express';
+import bodyParser from 'body-parser';
 import { Request as Req, Response as Res } from 'express';
+import { PORT } from './config';
+import { CredentialsPlus, CredentialsPremium, MessageCreate, PubsubRequest } from './interfaces';
+import { DocumentTier } from './boufin';
+import standardChoice from './choice/standard';
+import plusChoice from './choice/plus';
+import premiumChoice from './choice/premium';
+import { publish } from './pubsub';
+import { getDocument } from './firestore';
 
-export async function messageHandler(req: Req, res: Res) {
+const app = express();
+
+app.use(bodyParser.json());
+app.post('/create', async (req: Req, res: Res) => {
+  const response = { docId: '' };
   try {
-    const rawData = Buffer.from(req.body.message.data, 'base64');
-    const message = JSON.parse(rawData.toString('utf-8'));
-    console.info(`Report stored succesfully on ${message}!`);
+    const { tier, username, password } = req.body as MessageCreate;
+    let result: PubsubRequest;
+    if (tier === DocumentTier.STANDARD) {
+      result = standardChoice(username, password);
+    } else if (tier === DocumentTier.PLUS) {
+      result = plusChoice(username as CredentialsPlus, password as CredentialsPlus);
+    } else if (tier === DocumentTier.PREMIUM) {
+      result = premiumChoice(username as CredentialsPremium, password as CredentialsPremium);
+    } else {
+      throw new Error('Document tier is not available, may not be implemented.');
+    }
+    response.docId = await publish(result);
   } catch (error) {
     console.error(error);
   }
-  res.status(200).end();
-}
-console.info('Server started listening to topic...');
+  res.status(200).send(response);
+});
+app.get('/report/:docId([0-9a-fA-F]{64})', async (req: Req, res: Res) => {
+  const { docId } = req.params;
+  const response = { complete: false, results: {} };
+  try {
+    const report = await getDocument(docId);
+    response.complete = true;
+    response.results = report;
+  } catch (error) {
+    console.error(error);
+  }
+  res.status(200).send(response);
+});
+
+app.listen(PORT);
---
+++ src/interfaces.ts
@@ -0,0 +1,40 @@
+import { DocumentTier, EntityId } from './boufin';
+
+export type AnyObject = Record<string, unknown> | Record<string, never>;
+
+export interface Credentials {
+  unique: string;
+  bank?: EntityId;
+  servipag?: AnyObject;
+}
+
+export interface CredentialsPlus {
+  unique: string;
+  bank: EntityId;
+  servipag?: AnyObject;
+}
+
+export interface CredentialsPremium {
+  unique: string;
+  bank: EntityId;
+  servipag: AnyObject;
+}
+
+export interface MessageCreate {
+  tier: DocumentTier;
+  username: Credentials;
+  password: Credentials;
+}
+
+export interface MessageRecover {
+  reportId?: string;
+}
+
+export interface PubsubRequest {
+  docId: string;
+  data: Array<{
+    request: string;
+    username: string;
+    password: string;
+  }>;
+}
